496da4656b3aa3fa4add3802ebca691f2cf54a64
nero <support@mtmt.tech>
2023-08-21 21:06:57 +0800

    增加ocr相关代码

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -335,6 +335,11 @@ module.exports = function (webpackEnv) {
           babelRuntimeRegenerator,
         ]),
       ],
+      fallback: {
+        "path": false,
+        "fs": false,
+        "crypto": false,
+      }
     },
     module: {
       strictExportPresence: true,
@@ -419,7 +424,7 @@ module.exports = function (webpackEnv) {
                     },
                   ],
                 ],
-                
+
                 plugins: [
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
@@ -453,7 +458,7 @@ module.exports = function (webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
--- src/App.js
+++ src/App.js
@@ -6,24 +6,53 @@ import {useEffect, useState} from "react";
 function App() {
 
     const [loading, setLoading] = useState(false)
+    const [result, setResult] = useState("")
 
     useEffect(()=>{
 
+        const cvs=document.getElementById("cvs")
+        const img1=document.getElementById("img1")
+
+        setLoading(true)
         ocr.init().then((res) => {
-            console.log("res", res)
+
+            ocr.recognize(img1,{canvas:cvs}).then((res) => {
+                console.log("res", res)
+                setResult(JSON.stringify(res.text,null,2))
+            }).catch((e) => {
+                console.log("catch error:", e)
+            })
+
         }).catch((e) => {
             console.log("catch error:", e)
+        }).finally(()=>{
+            setLoading(false)
         })
 
 
     },[])
     return (
         <div className="App">
-            <p>{loading ? "加载模型中" : ""}</p>
+            <p style={{color:"#f00",fontSize:18}}>{loading ? "加载模型中" : ""}</p>
 
-            <canvas
+            <div style={{display:"flex",flexDirection:"row"}}>
+                <div>
+                    <p style={{color:"#f00",fontSize:18}}>原始图片：</p>
+                    <img  style={{width:540,height:1200}} id="img1" src="/test1.jpg" alt=""/>
+                </div>
+                <div style={{padding:"0 15px",width:540,}}>
+                    <p style={{color:"#f00",fontSize:18}}>识别框：</p>
+                    <canvas
+                        style={{width:540,height:1200}}
+                        id="cvs"
+                    />
+                </div>
+                <pre style={{color: "#f00", fontSize: 18, width: 300}}>识别结果：{result}</pre>
+            </div>
 
-            />
+
+
+
 
         </div>
     );
